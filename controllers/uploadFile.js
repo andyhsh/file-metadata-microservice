f6992baae7a7b123aead712a60f13f59cd7a712c
andyhsh <89andy@gmail.com>
2018-04-04 16:49:27 +0800

    Change to read only file

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: '/uploads' });
+const upload = multer();
 const router = express.Router();
 
 router.post('/get-file-size', upload.single('file'), (req, res) => {
